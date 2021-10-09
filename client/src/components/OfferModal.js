a460659bb424bbfab55adaf7378628c1675f5e4e
ShlomiTofahi <shlomitofahi@gmail.com>
2021-10-10 00:07:43 +0300

    .

--- client/src/components/OfferModal.js
+++ client/src/components/OfferModal.js
@@ -137,7 +137,7 @@ export default class Contact extends Component {
     render() {
         return (
             <div>
-                <button data-aos='fade-left' data-aos-delay="2000" className='offer-btn' onClick={this.toggle}>
+                <button data-aos='fade-left' data-aos-delay="2000" data-aos-anchor-placement="top-bottom" className='offer-btn' onClick={this.toggle}>
                     <span>להצעת מחיר</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
